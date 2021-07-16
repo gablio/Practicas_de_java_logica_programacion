4b04d83d044ed42323f36c62772eb8ca685034b4
Julio Gabriel Milano <gablio@live.com>
2021-07-16 02:53:12 -0300

    Comienzo de proyecto teclab

---
+++ src/animales/Jirafa.java
@@ -0,0 +1,10 @@
+package animales;
+
+public class Jirafa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/animales/test.java
@@ -0,0 +1,10 @@
+package animales;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
